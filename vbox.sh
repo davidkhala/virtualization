6dbcec7caf507fd81f09aaa9cd82b538ca09b4eb
David Liu <david-khala@hotmail.com>
2022-11-02 11:55:17 +0000

    add oci key gen

--- vbox.sh
+++ vbox.sh
@@ -1,5 +1,13 @@
 set -e
-create-oci-key(){
+create-oci-key() {
+
+    local privateKey=${1:-~/.oci/oci_api_key_vbox.pem}
+    local publicKey=${2:-~/.oci/oci_api_key_public_vbox.pem}
+    openssl genrsa -out $privateKey 2048
+    chmod go-rwx $privateKey
+    openssl rsa -pubout -in $privateKey -out $publicKey
+    
+    cat $publicKey | xclip
 
 }
 
