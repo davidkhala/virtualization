931192565e578ed155e1ce5d5d31115c4fa031f2
David Liu <david-khala@hotmail.com>
2022-11-23 13:16:18 +0800

    Update vbox.sh

--- vbox.sh
+++ vbox.sh
@@ -6,5 +6,11 @@ linux() {
     curl https://raw.githubusercontent.com/davidkhala/linux-utils/main/vbox.sh | bash -s create-oci-key $@
 
 }
+list-vm-all(){
+    vboxmanage list vms
+}
+list-vm(){
+    VBoxManage list runningvms
+}
 
 $@
