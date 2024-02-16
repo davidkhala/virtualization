51425e6adc8b9ef347ffb8473cae86d5ee17d56c
David Liu <david-khala@hotmail.com>
2024-02-16 19:39:25 +0800

    Update vbox-manage.sh

--- virtual-box/vbox-manage.sh
+++ virtual-box/vbox-manage.sh
@@ -5,13 +5,13 @@ list-vm() {
     vboxmanage list runningvms
 }
 start-vm() {
-    VBoxManage startvm $1
+    vboxmanage startvm $1
 }
 delete-vm() {
-    VBoxManage unregistervm --delete $1
+    vboxmanage unregistervm --delete $1
 }
 stop-vm() {
-    # TODO test
-    VBoxManage controlvm $1 acpipowerbutton
+    
+    vboxmanage controlvm $1 acpipowerbutton
 }
 $@
